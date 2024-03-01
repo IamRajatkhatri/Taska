ca0ef8644f2f5a8af4724cb11f26e029deed57f4
IamRajatkhatri <rajat.khatri.2609@gmail.com>
2024-03-01 14:13:41 +0530

    gheslj

---
+++ GitHubProject/src/test/java/constants/Constant.java
@@ -0,0 +1,14 @@
+package constants;
+
+public class Constant {
+
+	public static final String URL = "https://demoqa.com/automation-practice-form";
+    public static final String Path_TestData = "C:\\Users\\admin\\eclipse-workspace\\CucumberJava\\src\\test\\java\\TestData\\";
+    public static final String File_TestData = "testDataExcel.xlsx";
+    
+ 
+}
+	
+	
+	
+
